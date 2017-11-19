1c86af57b4d4581f147f7436fd8c753825610caf
alepmaros <alehstk@gmail.com>
2017-11-19 19:17:13 -0200

    Mensagens na tela de preferencias

--- criptosorteio/criptosorteio/forms.py
+++ criptosorteio/criptosorteio/forms.py
@@ -13,13 +13,16 @@ class RegisterForm(forms.Form):
         widget=forms.TextInput(attrs={'placeholder': 'Seu sobrenome...'}))
 
     username = forms.CharField(min_length=4, max_length=150, required=True, label="Usuario",
-        widget=forms.TextInput(attrs={'placeholder': 'Seu usuario...'}))
+        widget=forms.TextInput(attrs={'placeholder': 'Seu usuario...',
+                                      'render_value': False}))
 
     password = forms.CharField(required=True,
-        widget=forms.PasswordInput(attrs={'placeholder': 'Sua senha...'}))
+        widget=forms.PasswordInput(attrs={'placeholder': 'Sua senha...',
+                                          'render_value': False}))
 
     password_confirm = forms.CharField(required=True,
-        widget=forms.PasswordInput(attrs={'placeholder': 'Confirme sua senha...'}))
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirme sua senha...',
+                                          'render_value': False}))
 
     email = forms.EmailField(required=True, label="Email")
 
@@ -35,8 +38,7 @@ class RegisterForm(forms.Form):
     
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        username = self.cleaned_data.get('username')
-        if email and User.objects.filter(email=email).exclude(username=username).exists():
+        if email and User.objects.filter(email=email).exists():
             raise forms.ValidationError(_('Email ja utilizado.'))
         return email
 
@@ -58,19 +60,23 @@ class UpdateUserForm(forms.Form):
     email = forms.EmailField(required=True, label="Email")        
 
     current_password = forms.CharField(required=True, label="Senha Atual",
-        widget=forms.PasswordInput(attrs={'placeholder': 'Sua senha...'}))
+        widget=forms.PasswordInput(attrs={'placeholder': 'Sua senha...',
+                                          'render_value': False}))
 
     layout = Layout(Row('first_name', 'last_name'), 'email', 'current_password')
 
 class UpdatePasswordForm(forms.Form):
     current_password = forms.CharField(required=True, label="Senha Atual",
-        widget=forms.PasswordInput(attrs={'placeholder': 'Sua senha...'}))
+        widget=forms.PasswordInput(attrs={'placeholder': 'Sua senha...',
+                                          'render_value': False}))
 
     new_password = forms.CharField(required=True,
-        widget=forms.PasswordInput(attrs={'placeholder': 'Sua nova senha...'}))
+        widget=forms.PasswordInput(attrs={'placeholder': 'Sua nova senha...',
+                                          'render_value': False}))
 
     password_confirm = forms.CharField(required=True,
-        widget=forms.PasswordInput(attrs={'placeholder': 'Confirme sua senha...'}))
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirme sua senha...',
+                                          'render_value': False}))
 
     def clean(self):
         cleaned_data = super(UpdatePasswordForm, self).clean()
--- criptosorteio/criptosorteio/views.py
+++ criptosorteio/criptosorteio/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.models import User
 
 from .forms import RegisterForm, UpdateUserForm, UpdatePasswordForm
@@ -45,6 +47,8 @@ def preferencias_view(request):
                                                 'email'     : request.user.email})
     form_change_pw = UpdatePasswordForm()
 
+    err_update = err_change_pw = None
+
     if request.method == 'POST':
         if '_submit_update_user' in request.POST:
             form_general_pref = UpdateUserForm(request.POST)
@@ -55,14 +59,23 @@ def preferencias_view(request):
                 email = form_general_pref.cleaned_data['email']
                 current_password = form_general_pref.cleaned_data['current_password']
 
-                user = User.objects.get(username=request.user.username)
-                if (user.check_password(current_password)):
-                    user.first_name = first_name
-                    user.last_name = last_name
-                    user.email = email
-                    user.save()
+                # If the email is not being altered or if the email does not belong to another user
+                if (not User.objects.filter(email=email).exclude(username=request.user.username).exists()):
+                    user = User.objects.get(username=request.user.username)
+                    if (user.check_password(current_password)):
+                        user.first_name = first_name
+                        user.last_name = last_name
+                        user.email = email
+                        user.save()
+                        messages.add_message(request, messages.SUCCESS, 
+                            'Cadastro atualizado.', extra_tags='user_update')
+                        return HttpResponseRedirect('')
+                    else:
+                        messages.add_message(request, messages.ERROR,
+                            'Senha incorreta', extra_tags='user_update')
                 else:
-                    print("ERROR")
+                    messages.add_message(request, messages.ERROR,
+                        'E-mail ja utilizado', extra_tags='user_update')
 
         elif '_submit_change_pw' in request.POST:
             form_change_pw = UpdatePasswordForm(request.POST)
@@ -75,9 +88,13 @@ def preferencias_view(request):
                 if (user.check_password(current_password)):
                     user.set_password(new_password)
                     user.save()
-                    form_change_pw = UpdatePasswordForm()
+                    update_session_auth_hash(request, user)
+                    messages.add_message(request, messages.SUCCESS, 
+                            'Senha atualizada.', extra_tags='change_pw')
+                    return HttpResponseRedirect('')
                 else:
-                    print("ERROR")
+                    messages.add_message(request, messages.ERROR,
+                        'Senha atual incorreta', extra_tags='change_pw')
 
     return render(request, 'registration/preferences.html', {'form_general_pref': form_general_pref,
                                                              'form_change_pw'   : form_change_pw})
