74cf5e2d69378c81c1dac5b6b6f60b70ab576732
alepmaros <alehstk@gmail.com>
2017-11-20 10:56:16 -0200

    Impedir que crie sorteios no passado

--- criptosorteio/sorteios/forms.py
+++ criptosorteio/sorteios/forms.py
@@ -1,5 +1,6 @@
 from django.utils.translation import ugettext as _
 from django import forms
+from django.utils import timezone
 
 from django.template import Template
 from material import Layout, Row, Column, Fieldset
@@ -13,3 +14,9 @@ class SorteioForm(forms.ModelForm):
     class Meta:
         model = Sorteio
         fields = ('nome', 'descricao', 'foto', 'hora_sorteio', 'privacidade' )
+
+    def clean_hora_sorteio(self):
+        hora_sorteio = self.cleaned_data.get('hora_sorteio')
+        if hora_sorteio < timezone.now():
+            raise forms.ValidationError(_('Sorteio não pode ser no passado'))
+        return hora_sorteio
