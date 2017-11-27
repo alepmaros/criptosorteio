02eae2db6f7ac26612abeef2396aa94ee73270bb
alepmaros <alehstk@gmail.com>
2017-11-27 20:38:19 -0200

    Página inicial

--- criptosorteio/static/js/main.js
+++ criptosorteio/static/js/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     new SweetScroll({});particlesJS("particles-js",{
         "particles": {
         "number": {
-            "value": 80,
+            "value": 100,
             "density": {
             "enable": true,
             "value_area": 800
